3670518cc9fef765d94c1ed3c48fb9bb84f4901a
Andreayt0972 <89881184+Andreayt0972@users.noreply.github.com>
2022-04-27 18:43:27 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,8 +1,8 @@
 function handler(m) {
   this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-  this.sendContact(m.chat, '593993684821', 'OFICIAL - GataBot', m)
-  this.sendContact(m.chat, '14509251286', 'OFICIAL - SubGataBot', m)
-  this.sendContact(m.chat, '14509544207', 'Gata Dios', m)
+  this.sendContact(m.chat, '573125484672', 'OFICIAL - ANDREABot', m)
+  this.sendContact(m.chat, '573125484672', 'OFICIAL - SubANDREABot', m)
+  this.sendContact(m.chat, '573125484672', 'ANDREA Dios', m)
   }
 handler.help = ['contacto']
 handler.tags = ['info']
